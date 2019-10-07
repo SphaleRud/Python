037beeedcddbbc0a62466050c4218212b188ca67
SpaleRud <55204645+SpaleRud@users.noreply.github.com>
2019-10-07 22:16:41 +0700

    Delete УровеньС.py

--- ZakharkinMaksim/Вложенные циклы/УровеньС.py
+++
@@ -1,9 +0,0 @@
-a,b = map(int,input('Введите границы диапозона: ').split())
-for i in range(a, b+1): 
-    prime = True    
-    for j in range(a, i):
-        if i%j == 0:
-            prime = False
-            break 
-        elif prime:
-            print(i)
