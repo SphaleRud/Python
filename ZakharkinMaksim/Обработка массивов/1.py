8aa48499326740718e5c574d8ef7015667da25f5
SpaleRud <55204645+SpaleRud@users.noreply.github.com>
2019-11-13 22:52:53 +0700

    Update 1.py

--- ZakharkinMaksim/Обработка массивов/1.py
+++ ZakharkinMaksim/Обработка массивов/1.py
@@ -1,22 +1,28 @@
 from random import *
 c=[randint(-1000,1000) for i in range(20)]
-flag1=True
-flag1=True
+flag1=False
+flag2=False
+k=0
+v=0
 print(*c)
-print("A):",end=' ')
+print("\nA):",end=' ')
 for i in range(20):
-    if c[i]//10 < 10 and c[i]//10!=0 and c[i]//10 > -10:
+    if c[i]<10 or c[i]>=100:
+        k+=1
+    if abs(c[i])//10<10 and abs(c[i])//10!=0:
         print(c[i],end=' ')
-    else:
-        flag1=False
-print("\nB):",end=" ")
-for f in range(20):
-    if c[f]//100 < 10 and c[f]//100 !=-1 and c[f]//100 > -10:
-        print(c[f],end=' ')
-    else:
-        flag2=False
+    elif k==20:
+        flag1=True
 if flag1:
     print("Двухзначных чисел нет.")
+print("\nB):",end=" ")
+for f in range(20):
+    if c[i]<10 or c[i]>=100:
+        v+=1
+    if c[f]//100 < 10 and c[f]//100 > -11 and c[f]//100!=-1 and c[f]//100!=0:
+        print(c[f],end=' ')
+    elif v==20:
+        flag2=True
 if flag2:
     print("Трехзначных чисел нет.")
 
