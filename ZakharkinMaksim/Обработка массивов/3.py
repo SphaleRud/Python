47f0baa0290ff6110c836f37bd3f714dfefb168a
SpaleRud <55204645+SpaleRud@users.noreply.github.com>
2019-11-13 23:02:38 +0700

    Update 3.py

--- ZakharkinMaksim/Обработка массивов/3.py
+++ ZakharkinMaksim/Обработка массивов/3.py
@@ -1,10 +1,12 @@
 from random import *
 c=[randint(-1000,1000) for i in range(20)]
 print(*c,'\n')
+
+print("Номера элементов, которые оканчиваются на 4: ", end=" ")
 for i in range(20):
     if c[i] >= 0:
         if c[i]%10 == 4:
-            print(c[i])
+            print(i)
     elif c[i] < 0:
         if c[i]%10 == 6:
-            print(c[i])
+            print(i)
